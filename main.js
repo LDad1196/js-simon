5267c87b233e252fb93b0389e874fabbce7366fe
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-05 13:34:24 +0200

    M3 Completato

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let value3 = random();
 let value4 = random();
 let value5 = random();
 
+let punteggio = 0
 
 // Funzione che regola creazione numeri
 function random() {
@@ -16,10 +17,13 @@ function random() {
 
 // Aggiungere event listener che fa partire il gioco al click
 document.getElementById("start-btn").addEventListener ("click", function(){
-// Creare Timer di 30 secondi nel quale sono visibili i numeri
+    
+    // Creare Timer di 30 secondi nel quale sono visibili i numeri
     let seconds = 3;
+    
+    // Gestire funzione che permette di visualizzare i numeri nel momento in cui c'è la possibilità di vedere i numeri
     let timerNumbers = setInterval(function() {
-	    // Gestire funzione che permette di visualizzare i numeri nel momento in cui c'è la possibilità di vedere i numeri
+	    
         if(seconds!=0) {
 		    document.querySelector("h2").innerText = `I numeri sono: ${value1},  ${value2},  ${value3},  ${value4},  ${value5}`;
 		    seconds--;
@@ -28,12 +32,28 @@ document.getElementById("start-btn").addEventListener ("click", function(){
 		    clearInterval(timerNumbers);
 	    }
     }, 1000);
+    
+    // Inserire Prompt che ti permette di gestire inserimento numeri
+    let promptValue = setInterval(function() {
+        if(seconds == 0) {
+            let calcolo1 = parseInt (prompt("Inserisci il primo numero"))
+            console.log("Hai scelto il numero: ", calcolo1)
+            let calcolo2 = parseInt (prompt("Inserisci il secondo numero"))
+            console.log("Hai scelto il numero: ", calcolo2)
+            let calcolo3 = parseInt (prompt("Inserisci il terzo numero"))
+            console.log("Hai scelto il numero: ", calcolo3)
+            let calcolo4 = parseInt (prompt("Inserisci il quarto numero"))
+            console.log("Hai scelto il numero: ", calcolo4)
+            let calcolo5 = parseInt (prompt("Inserisci il quinto numero"))
+            console.log("Hai scelto il numero: ", calcolo5)
+            clearInterval(promptValue)
+        }
+    }, 1000)
 
-    if (timerNumbers == 0) {
-        let myNumber = parseInt(prompt("Inserisci i numeri"))
-    }
+    
+
 }, {once: true})
 
-// Inserire Prompt che ti permette di gestire inserimento numeri
+
 
 
